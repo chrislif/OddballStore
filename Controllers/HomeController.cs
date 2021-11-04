6fe225138e530c332ea3c10994e58911e76ed0eb
zi652223 <zi652223@southeast.edu>
2021-11-04 15:31:46 -0500

    updated nav

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,6 +28,16 @@ namespace OddballStore.Controllers
             return View();
         }
 
+        public IActionResult Items()
+        {
+            return View();
+        }
+
+        public IActionResult Cart()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
