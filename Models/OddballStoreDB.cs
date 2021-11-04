8cfa20cad6a75273cd74a0f43774aec097083e18
af738947 <af738947@LR00902>
2021-11-04 16:02:37 -0500

    Added DA and DB classes

---
+++ Models/OddballStoreDB.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OddballStore.Models
+{
+    public class OddballStoreDB
+    {
+        public static SqlConnection GetConnection()
+        {
+
+            string connectionString = ;
+
+            SqlConnection connection = new SqlConnection(@WebConfigurationManager.C);
+
+            return connection;
+        }
+    }
+}
