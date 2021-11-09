4ac83c6ce91eca036591ef749fdf9e96396c51da
k-saenz <ks689665@southeast.edu>
2021-11-09 15:57:44 -0600

    Display user name to page

--- Models/User.cs
+++ Models/User.cs
@@ -1,16 +1,12 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace OddballStore.Models
 {
-    public class User
+    public class User : IdentityUser
     {
-        public int UserID { get; set; }
-        public string Email { get; set; }
-        public string Phone { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
     }
 }
