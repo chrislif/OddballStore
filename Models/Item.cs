02cef5ab6cafb6b4ae9bf9b4bfcce4d5a39e2c98
af738947 <af738947@LR00902>
2021-11-11 13:48:10 -0600

    tweaking item

--- Models/Item.cs
+++ Models/Item.cs
@@ -11,12 +11,13 @@ namespace OddballStore.Models
         public string Name { get; set; }
         public string Description { get; set; }
         public int Price { get; set; }
+        public string Thumbnail { get; set; }
 
         public Item() { }
 
-        public Item(string name, string description, int price)
+        public Item(string name, string description, int price, string thumbnail)
         {
-
+            
         }
 
 
