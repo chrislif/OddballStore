58000818fbbb1ee01d728b3bff3aa0dd1a7bb278
k-saenz <ks689665@southeast.edu>
2021-11-23 15:08:21 -0600

    scaffold login page

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,21 +1,7 @@
 ﻿$(document).ready(() => {
 	const container = document.querySelector('.container');
 
-	document.addEventListener('mouseover', (e) => {
-		const eye = document.querySelectorAll('.eyes');
-		[].forEach.call(eye, function (eye) {
-			let mouseX = eye.getBoundingClientRect().right;
-			if (eye.classList.contains('eyes')) {
-				mouseX = eye.getBoundingClientRect().left;
-			}
-			let mouseY = eye.getBoundingClientRect().top;
-			let radianDegrees = Math.atan2(e.pageX - mouseX, e.pageY - mouseY);
-			let rotationDegrees = radianDegrees * (180 / Math.PI) * -1 + 180;
-			eye.style.transform = `rotate(${rotationDegrees}deg)`;
-		});
-	});
 
-	//ALT EYE SOLUTION
 	$(document).mousemove(function (event) {
 		var eye = $(".eyes");
 		var x = (eye.offset().left) + (eye.width() / 2);
